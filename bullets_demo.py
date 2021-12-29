a31a52d964d998f43e34ffb2a057af3f4bc2dc24
Edward <thebestoneslie@ya.ru>
2021-12-29 14:27:00 +0300

    Враг движется по заданной траектории

--- bullets_demo.py
+++ bullets_demo.py
@@ -7,6 +7,9 @@ screen = pygame.display.set_mode(SIZE)
 all_sprites = pygame.sprite.Group()
 
 
+# используемые на данный момент спрайты будут заменены
+# как только всё остальное будет работать
+# пока что здесь первые попавшиеся картинки из интернета
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
@@ -47,10 +50,36 @@ class Hero(pygame.sprite.Sprite):
         self.rect.y += movement[1] * self.v
 
 
+class MeleeEnemy(pygame.sprite.Sprite):
+    image = pygame.transform.scale(load_image('test_melee_enemy.png'), (30, 30))
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = MeleeEnemy.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 300
+        self.rect.y = 150
+        self.v = 2
+        # первое число -- направление по X, второе -- по Y
+        # третье -- время в секундах на это движение
+        self.path = [(1, 0, 2), (-1, 0, 2)]
+        self.current_phase = 0
+        self.phase_started = pygame.time.get_ticks()
+
+    def move(self):
+        movement = self.path[self.current_phase]
+        self.rect.x += self.v * movement[0]
+        self.rect.y += self.v * movement[1]
+        if pygame.time.get_ticks() - self.phase_started >= movement[2] * 1000:
+            self.current_phase = (self.current_phase + 1) % len(self.path)
+            self.phase_started = pygame.time.get_ticks()
+
+
 def main():
     pygame.init()
 
     hero = Hero(all_sprites)
+    enemy = MeleeEnemy(all_sprites)
     fps = 40
     clock = pygame.time.Clock()
     running = True
@@ -58,8 +87,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        enemy.move()
         hero.move(pygame.key.get_pressed())
-        screen.fill((0, 0, 0))
+        screen.fill((255, 255, 255))
         all_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(fps)
